8e165ebfde22bade937405ccd9e324ea7a4250ac
Mattia Roccoberton <mattia.roccoberton@develondigital.com>
2017-10-25 22:46:04 +0200

    Minor improvements

--- db/migrate/20170414173610_create_contents_core_items.rb
+++ db/migrate/20170414173610_create_contents_core_items.rb
@@ -1,8 +1,8 @@
 class CreateContentsCoreItems < ActiveRecord::Migration[5.0]
   def change
     create_table :contents_core_items do |t|
-      t.string :type
-      t.string :name, null: false, default: 'data'
+      t.string  :type
+      t.string  :name, null: false, default: 'data'
       t.integer :block_id
     end
 

2871c7e69fd96b4e2323a7999ebecd58590d68d4
Mattia Roccoberton <mattia.roccoberton@develondigital.com>
2017-11-01 09:27:35 +0100

    Improved tests

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -16,15 +16,59 @@ module ContentsCore
       assert_equal item.name, 'a-block'
     end
 
-    test 'should create a text item' do
-      item = @page.create_block.create_item 'ContentsCore::ItemText', 'a-block'
-      item.set 'Some text'
+    # test 'should create an array item' do
+    #   item = @page.create_block.create_item 'ContentsCore::ItemArray', 'a-block'
+    #   # TODO
+    # end
+
+    test 'should create a boolean item' do
+      item = @page.create_block.create_item 'ContentsCore::ItemBoolean', 'a-block'
+      item.set true
       item.save
-      item = ItemText.find_by name: 'a-block'
-      data = item.read_attribute( :data_text )
-      assert_equal data, 'Some text'
+      item = ItemBoolean.find_by name: 'a-block'
+      data = item.read_attribute( :data_boolean )
+      assert_equal data, true
+      assert_equal item.data, true  # test alias
+      assert_equal data.class, TrueClass
+    end
+
+    test 'should create a datetime item' do
+      dt = DateTime.now - 1440
+      item = @page.create_block.create_item 'ContentsCore::ItemDatetime', 'a-block'
+      item.set dt
+      item.save
+      item = ItemDatetime.find_by name: 'a-block'
+      data = item.read_attribute( :data_datetime )
+      assert_equal data, dt
+      assert_equal item.data, dt  # test alias
+    end
+
+    test 'should create a file item' do
+      item = @page.create_block.create_item 'ContentsCore::ItemFile', 'a-block'
+      item.set 'a-filename'
+      item.save
+      item = ItemFile.find_by name: 'a-block'
+      data = item.read_attribute( :data_file )
+      assert_equal data, 'a-filename'
+      assert_equal item.data, 'a-filename'  # test alias
       assert data.is_a?( String )
     end
+
+    test 'should create a float item' do
+      item = @page.create_block.create_item 'ContentsCore::ItemFloat', 'a-block'
+      item.set 12.34
+      item.save
+      item = ItemFloat.find_by name: 'a-block'
+      data = item.read_attribute( :data_float )
+      assert_equal data, 12.34
+      assert_equal item.data, 12.34  # test alias
+      assert data.is_a?( Float )
+    end
+
+    # test 'should create an hash item' do
+    #   item = @page.create_block.create_item 'ContentsCore::ItemHash', 'a-block'
+    #   # TODO
+    # end
 
     test 'should create an integer item' do
       item = @page.create_block.create_item 'ContentsCore::ItemInteger', 'a-block'
@@ -33,7 +77,33 @@ module ContentsCore
       item = ItemInteger.find_by name: 'a-block'
       data = item.read_attribute( :data_integer )
       assert_equal data, 12
-      assert data.is_a?( Fixnum )
+      assert_equal item.data, 12  # test alias
+      assert_equal data.class, Fixnum
+    end
+
+    # test 'should create an object item' do
+    #   # TODO
+    # end
+
+    test 'should create a string item' do
+      item = @page.create_block.create_item 'ContentsCore::ItemString', 'a-block'
+      item.set 'A test string'
+      item.save
+      item = ItemString.find_by name: 'a-block'
+      data = item.read_attribute( :data_string )
+      assert_equal data, 'A test string'
+      assert_equal item.data, 'A test string'  # test alias
+      assert data.is_a?( String )
+    end
+
+    test 'should create a text item' do
+      item = @page.create_block.create_item 'ContentsCore::ItemText', 'a-block'
+      item.set 'Some text'
+      item.save
+      item = ItemText.find_by name: 'a-block'
+      data = item.read_attribute( :data_text )
+      assert_equal data, 'Some text'
+      assert data.is_a?( String )
     end
 
     test 'should not create an item without a block' do

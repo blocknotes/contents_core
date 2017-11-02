611e364a686ae5993c4f10a68be24955356e6ff4
Mattia Roccoberton <mattia.roccoberton@develondigital.com>
2017-11-02 22:06:55 +0100

    Improved tests

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -17,12 +17,10 @@ module ContentsCore
     end
 
     test 'should create an array item' do
-      item = @page.create_block.create_item :item_array, 'an-item'
-      item.set 5
+      item = @page.create_block.create_item :item_array, 'an-item', 5
       item.save
       item = ItemArray.find_by name: 'an-item'
-      data = item.read_attribute( :data_integer )
-      assert_equal data, 5
+      assert_equal item.read_attribute( :data_integer ), 5
       assert_equal item.data, 5
     end
 
@@ -31,8 +29,7 @@ module ContentsCore
       item = ContentsCore::ItemArray.first
       item.set 2
       assert_equal item.enum, [['First', 1], ['Second', 2], ['Third', 3]]
-      data = item.read_attribute( :data_integer )
-      assert_equal data, 2
+      assert_equal item.read_attribute( :data_integer ), 2
       assert_equal item.data, 2
     end
 
@@ -44,8 +41,7 @@ module ContentsCore
       item.set true
       item.save
       item = ItemBoolean.find_by name: 'an-item'
-      data = item.read_attribute( :data_boolean )
-      assert_equal data, true
+      assert_equal item.read_attribute( :data_boolean ), true
       assert_equal item.data, true  # test alias
     end
 
@@ -55,8 +51,7 @@ module ContentsCore
       item.set dt
       item.save
       item = ItemDatetime.find_by name: 'an-item'
-      data = item.read_attribute( :data_datetime )
-      assert_equal data.to_date, dt.to_date
+      assert_equal item.read_attribute( :data_datetime ).to_date, dt.to_date
       assert_equal item.data.to_date, dt.to_date  # test alias
       # TODO: check me
       # assert_equal data, dt
@@ -68,8 +63,7 @@ module ContentsCore
       item.set 'a-filename'
       item.save
       item = ItemFile.find_by name: 'an-item'
-      data = item.read_attribute( :data_file )
-      assert_equal data, 'a-filename'
+      assert_equal item.read_attribute( :data_file ), 'a-filename'
       assert_equal item.data, 'a-filename'  # test alias
     end
 
@@ -78,8 +72,7 @@ module ContentsCore
       item.set 12.34
       item.save
       item = ItemFloat.find_by name: 'an-item'
-      data = item.read_attribute( :data_float )
-      assert_equal data, 12.34
+      assert_equal item.read_attribute( :data_float ), 12.34
       assert_equal item.data, 12.34  # test alias
     end
 
@@ -89,47 +82,44 @@ module ContentsCore
       item.set v
       item.save
       item = ItemHash.find_by name: 'an-item'
-      data = item.read_attribute( :data_hash )
-      assert_equal data, v
+      assert_equal item.read_attribute( :data_hash ), v
       assert_equal item.data, v  # test alias
     end
 
     test 'should create an integer item' do
-      item = @page.create_block.create_item :item_integer, 'an-item'
-      item.set 12
+      item = @page.create_block.create_item :item_integer, 'an-item', 12
       item.save
       item = ItemInteger.find_by name: 'an-item'
-      data = item.read_attribute( :data_integer )
-      assert_equal data, 12
+      assert_equal item.read_attribute( :data_integer ), 12
       assert_equal item.data, 12  # test alias
+      item.set 15
+      item.save
+      item = ItemInteger.find_by name: 'an-item'
+      assert_equal item.read_attribute( :data_integer ), 15
+      assert_equal item.data, 15
     end
 
     test 'should create an object item' do
       v = {a_key: 'A value', another_key: {a_sub_key: 'Another value'}}
-      item = @page.create_block.create_item :item_object, 'an-item'
-      item.set v
+      item = @page.create_block.create_item :item_object, 'an-item', v
       item.save
       item = ItemObject.find_by name: 'an-item'
       assert_equal item.data, v
     end
 
     test 'should create a string item' do
-      item = @page.create_block.create_item :item_string, 'an-item'
-      item.set 'A test string'
+      item = @page.create_block.create_item :item_string, 'an-item', 'A test string'
       item.save
       item = ItemString.find_by name: 'an-item'
-      data = item.read_attribute( :data_string )
-      assert_equal data, 'A test string'
+      assert_equal item.read_attribute( :data_string ), 'A test string'
       assert_equal item.data, 'A test string'  # test alias
     end
 
     test 'should create a text item' do
-      item = @page.create_block.create_item :item_text, 'an-item'
-      item.set 'Some text'
+      item = @page.create_block.create_item :item_text, 'an-item', 'Some text'
       item.save
       item = ItemText.find_by name: 'an-item'
-      data = item.read_attribute( :data_text )
-      assert_equal data, 'Some text'
+      assert_equal item.read_attribute( :data_text ), 'Some text'
     end
 
     test 'should not create an item without a block' do

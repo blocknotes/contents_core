b8d8bf1fca728e42f7cab2e81b9197226ef4bb17
Mattia Roccoberton <mattia.roccoberton@develondigital.com>
2017-11-23 15:13:09 +0100

    🚨  Improved tests

--- test/controllers/view_items_test.rb
+++ test/controllers/view_items_test.rb
@@ -8,16 +8,28 @@ module ContentsCore
 
     test 'should print all items of a text block' do
       @page.create_block :text, values: {title: 'A title', content: 'Some content'}
-      get page_path( @page )
+      assert_equal 200, get( page_path( @page ) )  # @response.body
       assert_select '#blocks .item.title', 'A title'
       assert_select '#blocks .item.content', 'Some content'
     end
 
-    # test 'should print all items of a slider block' do
-    #   @page.create_block :slider, create_children: 2 # , values: {title: 'A title', content: 'Some content'}
-    #   get page_path( @page )
-    #   # assert_select '#blocks .item.title', 'A title'
-    #   # assert_select '#blocks .item.content', 'Some content'
-    # end
+    test 'should print all items of a slider block' do
+      @page.create_block :slider, name: 'a-slider', create_children: 2 # , values: {title: 'A title', content: 'Some content'}
+      block = @page.get_block 'a-slider'
+      block.set 'slide.title', 'A title'
+      block.set 'slide.img', 'An image'
+      block.set 'slide-1.title', 'Another title'
+      block.set 'slide-1.img', 'Another image'
+      assert_equal 200, get( page_path( @page ) )
+
+      # <div class=\"block slide\"> <div class=\"item file img\"></div> <div class=\"item string title\"></div> </div> <div class=\"block slide-1\"><div class=\"block slide\"> <div class=\"item file img\"></div> <div class=\"item string title\"></div> </div> <div class=\"item file img\"></div> <div class=\"item string title\"></div> </div>
+
+      #   # "<div class=\"item file img\">An image</div><div class=\"item string title\">A title</div><div class=\"item file img\">An image</div><div class=\"item string title\">A title</div><div class=\"item file img\">Another image</div><div class=\"item string title\">Another title</div>"
+
+      # binding.pry
+
+      # assert_select '#blocks .item.title', 'A title'
+      # assert_select '#blocks .item.content', 'Some content'
+    end
   end
 end
